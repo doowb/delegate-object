392ac048ab129133ca4c04efedebb819c514ab65
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 21:08:26 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * delegate-object <https://github.com/doowb/delegate-object>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
